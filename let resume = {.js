997da860e134d129a76a5acca309498c0465af43
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-02-07 23:25:37 -0800

    Add files via upload

---
+++ let resume = {.js
@@ -0,0 +1,43 @@
+let resume = {
+    "name": "Dinesh",
+    "DOB": 7/12/2001,
+    "Father name": "M. karuppasamy",
+    "Mother name": "sundrapandiyammal",
+    "education": "B.SC",
+    "Hobbies": "kabbadi",
+    "skill": "typing",
+
+     
+}
+console.log (resume)
+
+
+
+_________________________________________________________________________________________________
+
+
+let notes = {
+    name : 'lokesh',
+    pages: 40,
+    price: 25,
+    isavailable: true,
+    frontpage:(index),
+
+}
+
+forinloop;
+ for (let key in notes) {
+    console.log(key , ":" ,notes (key));
+ }
+
+ forofloop;
+ let entries = object.entries(notes);
+    (let  = entries) ; {
+        console.log(entry[0], ":", entry[1]);
+    }
+
+forEachloop;
+let  = object.entries(notes);
+    entries.forEach(entry =>{
+        console.log(entry[0], ":", entry[1]);
+    });
